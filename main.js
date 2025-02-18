02dc1a9ab546ab5de13398906c891808d95c62b8
Michael <michael.ramirez2024@hilasal.superate.org.sv>
2025-02-18 08:44:41 -0600

    add variablen ✨

---
+++ main.js
@@ -0,0 +1,11 @@
+console.log("Hola mundo otra vez")
+
+let name = "KIKE";//string //:)
+let age = 16; //number //:)
+let isStudent = true; //bool //:)
+let direction = null;//object
+let phone ;//undefine
+let hobbies = ["read", "play", "eat"] //object
+let person = {name:"Carlos", age:15} //object //:)
+
+console.log(typeof age);
