43ef5a8c1c98ef7ebb8aa54d103ead550c9e3d65
Michael <michael.ramirez2024@hilasal.superate.org.sv>
2025-02-18 09:30:11 -0600

    avanzando con todo :construction_worker:

---
+++ Conditional.js
@@ -0,0 +1,14 @@
+let num =  prompt("Infresa un numero")
+num == Number(num);
+
+if(num >= 18){
+    alert("Es mayor de edad")
+
+} else if(num == 15){
+    ("Ta chiquito y tiene 15")
+}
+else{
+    alert("Es menor de edad")
+}
+
+
---
+++ DOM.js
@@ -0,0 +1,4 @@
+var element = document.getElementByIdElement("text");
+
+element.innerHTML = "<a>Ahora soy un link</a>";
+element.innerHTML = "Ahora soy un texto normal";
